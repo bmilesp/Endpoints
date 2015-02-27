8e12725643d0787aaf036c3618b8198b5b0fd682
bmilesp <bmilesp@gmail.com>
2015-02-27 10:17:30 -0500

    endpoints fix

--- Model/Behavior/EndpointBehavior.php
+++ Model/Behavior/EndpointBehavior.php
@@ -7,16 +7,14 @@ class EndpointBehavior extends ModelBehavior {
 
 	public $http = 'http://';
 
-	public function setup($Model, $config = array()){
+
+	public function callEndpoint(&$Model, $path= null, $type = 'POST', $data = array(), $options = array()){
 		$db = ConnectionManager::getDataSource($Model->useDbConfig);
 		$this->endpointHost = $db->config['host'];
 		$this->http = !empty( $db->config['https'])? 'https://' : 'http://';
-		parent::setup($Model, $config);
-	}
-
-	public function callEndpoint(&$Model, $path= null, $type = 'POST', $data = array(), $options = array()){
 		$fullPath = $this->http.$this->endpointHost . $path;
-		
+		//debug($Model->useDbConfig);
+		//debug($fullPath);
 		//$request = new CakeRequest($fullPath);
 		$http = new HttpSocket();
 		if($type == 'POST'){
